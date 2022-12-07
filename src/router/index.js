656493a05760306d307c7dd5f312768e5c9c940f
andikarafon <andika.rafon.senohaji@gmail.com>
2022-12-07 16:23:03 +0700

    Perlu Perbaikan di Product Thumbnails

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,8 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
   {
-    path: '/product',
+    path: '/product/:id',
+    props: true,
     name: 'product',
     component: Product
   },
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -26,42 +26,34 @@
                                   <div class="col-lg-6">
                                       <div class="product-pic-zoom">
                                           <Carousel id="gallery" :items-to-show="1" :wrap-around="false" v-model="currentSlide">
-                                            <Slide v-for="slide in slides" :key="slide.id">
+                                            <Slide v-for="ss in productDetails.galleries" :key="ss.id">
                                               <div class="carousel__item">
-                                                <img class="product-big-img" :src="slide.img_url" alt="" />
+                                                <img class="product-big-img" :src="ss.photo" alt="" />
                                               </div>
                                             </Slide>
                                           </Carousel>
                                       </div>
                                       <div class="product-thumbs">
                                         <carousel id="thumbnails" :items-to-show="3" :wrap-around="true" v-model="currentSlide" ref="carousel">
-                                          <slide v-for="slide in slides" :key="slide.id">
+                                          <slide v-for="ss in productDetails.galleries" :key="ss.id" class="pt">
                                             <div class="carousel__item">
-                                              <div class="pt" @click="slideTo(slide.id)" :class="slide.id == currentSlide ? 'active' : '' ">
-                                                 <img :src="slide.img_url" alt="" />
+                                              <div class="pt" @click="slideTo(ss.id)" :class="ss.id == currentSlide ? 'active' : '' ">
+                                                 <img :src="ss.photo" alt="" />
                                              </div>
                                             </div>
                                           </slide>
                                         </carousel>
                                       </div>
-                                  </div>
+                                </div>
                                   <div class="col-lg-6">
                                       <div class="product-details text-left">
                                           <div class="pd-title">
-                                              <span>oranges</span>
-                                              <h3>Pure Pineapple</h3>
+                                              <span>{{ productDetails.type }}</span>
+                                              <h3>{{ productDetails.name }}</h3>
                                           </div>
                                           <div class="pd-desc">
-                                              <p>
-                                                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error officia. Rem aperiam laborum voluptatum vel, pariatur modi hic provident eum iure natus quos non a sequi, id accusantium! Autem.
-                                              </p>
-                                              <p>
-                                                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam possimus quisquam animi, commodi, nihil voluptate nostrum neque architecto illo officiis doloremque et corrupti cupiditate voluptatibus error illum. Commodi expedita animi nulla aspernatur.
-                                                  Id asperiores blanditiis, omnis repudiandae iste inventore cum, quam sint molestiae accusamus voluptates ex tempora illum sit perspiciatis. Nostrum dolor tenetur amet, illo natus magni veniam quia sit nihil dolores.
-                                                  Commodi ratione distinctio harum voluptatum velit facilis voluptas animi non laudantium, id dolorem atque perferendis enim ducimus? A exercitationem recusandae aliquam quod. Itaque inventore obcaecati, unde quam
-                                                  impedit praesentium veritatis quis beatae ea atque perferendis voluptates velit architecto?
-                                              </p>
-                                              <h4>$495.00</h4>
+                                            <p v-html="productDetails.description"></p>
+                                            <h4>${{ productDetails.price }}</h4>
                                           </div>
                                           <div class="quantity">
                                             <router-link v-bind:to="'/cart'"  class="primary-btn pd-cart">Add To Cart</router-link>
@@ -88,6 +80,8 @@
 
   import "vue3-carousel/dist/carousel.css";
   import { Carousel, Slide } from 'vue3-carousel';
+
+  import axios from "axios";
   
   
   
@@ -99,34 +93,35 @@
       RelatedShayna,
       Carousel, Slide
     },
-    data: function() {
-        return {
-          slides: [
-            {
-              id: 0,
-              img_url : 'img/mickey1.jpg'
-            },
-            {
-              id: 1,
-              img_url : 'img/mickey2.jpg'
-            },
-            {
-              id: 2,
-              img_url : 'img/mickey3.jpg'
-            },
-            {
-              id: 3,
-              img_url : 'img/mickey4.jpg'
-            }
-          ],
-          currentSlide: 0
-        };
-  },  //end of data function
-    methods: {
-      slideTo(val) {
-        this.currentSlide = val
-      },
-    }
+                    data () {
+                      return {
+                        productDetails: [],
+                        currentSlide: 0
+                      };
+                    },  //end of data function
+                    methods: {
+                      slideTo(val) {
+                        this.currentSlide = val
+                      },
+                      setDataPicture(data) {
+                        // replace object productDetails dengan data dari API
+                        this.productDetails = data;
+                        // replace value gambar default dengan data dari API (galleries)
+                        // this.gambar_default = data.galleries[0].photo;
+                      }
+                    },
+                    mounted() {
+                                axios
+                                  .get("http://shayna-backend.belajarkoding.com/api/products", {
+                                    params: {
+                                      id: this.$route.params.id
+                                    }
+                                  })
+                                  //jika berhasil mengakses API-nya maka akan dijalankan perintah di bawah ini
+                                  .then(res => this.setDataPicture(res.data.data))
+                                  // eslint-disable-next-line no-console
+                                  .catch(err => console.log(err));
+                              }
   }
   </script>
   
