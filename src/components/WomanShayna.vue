47888fc53c4c422d8bc4e80dc3ef8152436523e3
andikarafon <andika.rafon.senohaji@gmail.com>
2022-12-12 09:39:05 +0700

    Woman shayna add to cart

--- src/components/WomanShayna.vue
+++ src/components/WomanShayna.vue
@@ -4,7 +4,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12 mt-5" v-if="products.length > 0" > 
-                    <carousel items-to-show="3"  :autoplay="2000" :wrap-around="true">
+                    <carousel items-to-show="3"  :wrap-around="true">
                         <slide v-for="itemProduct in products" v-bind:key="itemProduct.id">
                             <div class="carousel__item">
                                 <div class="single-hero-items set-bg">
@@ -12,8 +12,10 @@
                                         <div class="pi-pic">
                                             <img v-bind:src="itemProduct.galleries[0].photo" alt />
                                             <ul>
-                                              <li class="w-icon active">
-                                                  <a href="#"><i class="icon_bag_alt"></i></a>
+                                              <li @click="saveKeranjang(itemProduct.id, itemProduct.name, itemProduct.price, itemProduct.galleries[0].photo)" class="w-icon active">
+                                                <a href="#">
+                                                  <i class="icon_bag_alt"></i>
+                                                </a>
                                               </li>
                                                 <li class="quick-view">
                                                     <router-link v-bind:to="'/product/'+itemProduct.id">+ Quick View</router-link>
@@ -65,7 +67,8 @@ export default {
   },
   data() {
     return {
-      products: []
+      products: [],
+      keranjangUser: []
     };
   }, 
   mounted() {
@@ -74,6 +77,20 @@ export default {
       .then(res => (this.products = res.data.data.data))
       // eslint-disable-next-line no-console
       .catch(err => console.log(err));
+  },
+  methods: {
+    saveKeranjang(idProduct, nameProduct, priceProduct, photoProduct) {
+      var productStored = {
+        id: idProduct,
+        name: nameProduct,
+        price: priceProduct,
+        photo: photoProduct
+      };
+
+      this.keranjangUser.push(productStored);
+      const parsed = JSON.stringify(this.keranjangUser);
+      localStorage.setItem("keranjangUser", parsed);
+    }
   }
 }
 </script>
